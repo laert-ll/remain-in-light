f553bfc722bc4540aecc59fa6a755428a1a80722
Laert Llaveshi <l.llaveshi@tum.de>
2026-09-04 20:03:23 +0200

    Set up CMake build with OpenCV and dlib

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <opencv2/opencv.hpp>
+#include <dlib/image_processing/frontal_face_detector.h>
+#include <dlib/image_processing.h>
+#include <dlib/opencv.h>
+
+using namespace std;
+
+int main()
+  {
+    return 0;
+  }
